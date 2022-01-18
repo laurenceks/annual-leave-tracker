21aa8ce649fcd8e14be5050f591e5fd7ec92aeb3
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-01-18 15:55:16 +0000

    Reverted to normal bootstrap until style chosen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {createContext, useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootswatch/dist/yeti/bootstrap.min.css';
 import './App.css';
 import Main from "./components/Main";
 import NotAuthorised from "./components/NotAuthorised";
