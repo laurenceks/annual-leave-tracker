57a99e9961b5efa722b4dff0cd6b05e9633e8292
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-01-18 18:36:21 +0000

    Added more formats

--- src/functions/formatMySqlTimestamp.js
+++ src/functions/formatMySqlTimestamp.js
@@ -1,4 +1,13 @@
-const timestamp = (timestamp = '') => {
-    return `${timestamp.substr(11,8)} ${timestamp.substr(8,2)}/${timestamp.substr(5,2)}/${timestamp.substr(2,2)}`
+export const timestamp = (timestamp = "") => {
+    return `${timestamp.substr(11, 8)} ${timestamp.substr(8, 2)}/${timestamp.substr(5, 2)}/${timestamp.substr(2, 2)}`
 };
+
+export const timestampToTime = (timestamp = "") => {
+    return timestamp.substr(11, 8);
+};
+
+export const timestampToDate = (timestamp = "") => {
+    return `${timestamp.substr(8, 2)}/${timestamp.substr(5, 2)}/${timestamp.substr(2, 2)}`
+};
+
 export default timestamp;
