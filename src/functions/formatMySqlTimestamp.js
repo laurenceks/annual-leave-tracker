191280e632a6c4144f8ef0007da9d79457b79abe
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-01-20 12:09:40 +0000

    Added YYYY-MM-DD to DD/MM/YYYY formating function

--- src/functions/formatMySqlTimestamp.js
+++ src/functions/formatMySqlTimestamp.js
@@ -6,6 +6,12 @@ export const timestampToTime = (timestamp = "") => {
     return timestamp.substr(11, 8);
 };
 
+export const dateToShortDate = (date = "") => {
+    return date && date.length > 8 ?
+        `${date.substr(8, 2)}/${date.substr(5, 2)}/${date.substr(2, 2)}` :
+        "";
+};
+
 export const timestampToDate = (timestamp = "") => {
     return `${timestamp.substr(8, 2)}/${timestamp.substr(5, 2)}/${timestamp.substr(2, 2)}`
 };
