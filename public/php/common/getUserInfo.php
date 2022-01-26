5428e7ce18b52bbbbe4c2cf58c6be26cc2988b3e
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-01-26 18:44:53 +0000

    User info now returns paygrade and location names

--- public/php/common/getUserInfo.php
+++ public/php/common/getUserInfo.php
@@ -3,11 +3,13 @@ function getUserInfo($userId, $auth = null) {
     if ($userId) {
         require "db.php";
         $getUserInfo = $db->prepare("
-        SELECT users.email, users_info.*, users_info.userId as id, users_organisations.organisation
-        FROM users
-        LEFT JOIN users_info ON users.id = users_info.userId
-        LEFT JOIN users_organisations ON users_info.organisationId = users_organisations.id
-        WHERE users.id = :userId
+        SELECT `users`.`email`, `users_info`.*, `users_info`.`userId` AS `id`, `users_organisations`.`organisation`, `pay_grades`.`name` AS `payGradeName`, `locations`.`name` AS `locationName`
+        FROM `users`
+        LEFT JOIN `users_info` ON `users`.`id` = `users_info`.`userId`
+        LEFT JOIN `users_organisations` ON `users_info`.`organisationId` = `users_organisations`.`id`
+        LEFT JOIN `pay_grades` ON `users_info`.`payGradeId` = `pay_grades`.`id`
+        LEFT JOIN `locations` ON `users_info`.`locationId` = `locations`.`id`
+        WHERE `users`.`id` = :userId
         ");
         $getUserInfo->bindParam(':userId', $userId);
         $getUserInfo->execute();
