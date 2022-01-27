c005cfdfa8b49bde7f5ecf4550f2f8709443c97a
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-01-27 11:46:29 +0000

    Made policy more explicit and specific

--- src/components/login/policies/TextPage.js
+++ src/components/login/policies/TextPage.js
@@ -5,11 +5,12 @@ const TextPage = ({
                       children,
                       previousRoute
                   }) => {
-    return (<div className="position-fixed w-100 h-100 overflow-scroll top-0 start-0 p-4 p-md-5 bg-white">
-        {children}
-        <div className="text-center">
-            <NavLink className="btn btn-primary my-5 w-100" to={previousRoute || "/"} style={{maxWidth: "450px"}}>Go
-                back</NavLink>
+    return (<div className="position-fixed w-100 h-100 top-0 start-0 p-4 p-md-5 bg-white" style={{overflowY: "scroll"}}>
+        <div className="main m-auto">{children}
+            <div className="text-center">
+                <NavLink className="btn btn-primary my-5 w-100" to={previousRoute || "/"} style={{maxWidth: "450px"}}>Go
+                    back</NavLink>
+            </div>
         </div>
     </div>);
 };
