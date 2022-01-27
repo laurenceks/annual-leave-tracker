fba9fd7bd83d552c6341bff9dd567152aec1e5e2
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-01-27 12:34:32 +0000

    Fixed further issues with layout not sizing correctly accross devices

--- src/components/NotAuthorised.js
+++ src/components/NotAuthorised.js
@@ -10,40 +10,43 @@ import PrivacyPolicy from "./login/policies/PrivacyPolicy";
 const NotAuthorised = () => {
 
     return (<div className="container min-vh-100">
-        <div className="row justify-content-center align-content-md-center align-content-start h-100 w-100">
-            <div className="col col-12 col-md-4 my-3 my-md-0 mx-md-5 d-none d-md-flex align-items-center justify-content-center h-auto" style={{width:"33%"}}>
+        <div className="row justify-content-center align-content-md-center align-content-start h-100">
+            <div className="col col-12 col-md-5 d-none d-md-flex align-items-center justify-content-end h-auto">
                 <img src="./img/logo/logo.svg"
                      alt="Annual Leave Tracker logo"
                      className="w-100 h-100 loginLogo"
-                     style={{maxWidth: "185px"}}
+                     style={{
+                         maxWidth: "200px"
+                     }}
                 />
             </div>
-            <div className="col col-12 col-md-4 my-3 my-md-0 mx-md-5 d-flex d-md-none align-items-center justify-content-center h-auto" style={{maxWidth: "300px"}}>
+            <div className="col col-12 col-md-5 d-flex d-md-none align-items-center justify-content-center h-auto">
                 <img src="./img/logo/logoWide.svg"
                      alt="Annual Leave Tracker logo"
-                     className="w-100 h-100 loginLogo mb-3"
-
+                     className="w-100 h-100 loginLogo my-4"
+                     style={{maxWidth: "300px"}}
                 />
             </div>
-            <div className="col col-12 col-md-8 loginForm">
-                <Routes>
-                    <Route path="/verify" element={<Verify/>}/>
-                    <Route path="/reVerify" element={<ReVerify/>}/>
-                    <Route path="/register" element={<Register/>}/>
-                    <Route path="/forgotPassword" element={<ForgotPassword/>}/>
-                    <Route path="/resetPassword" element={<Verify type={"password"}/>}/>
-                    <Route path="/login" element={<Login/>}/>
-                    <Route path="/privacy"
-                           element={<TextPage>
-                               <PrivacyPolicy
-                                   appName={"Annual Leave Tracker"}
-                                   url={window.location.pathName}
-                                   websiteName={"Annual Leave Tracker"}
-                                   email={"help@annual-leave-tracker.com"}
-                               />
-                           </TextPage>}/>
-                    <Route path="*" element={<Navigate to="/login"/>}/>
-                </Routes>
+            <div className="col col-12 col-md-7 justify-content-center">
+                <div className="loginForm px-md-3">
+                    <Routes>
+                        <Route path="/verify" element={<Verify/>}/>
+                        <Route path="/reVerify" element={<ReVerify/>}/>
+                        <Route path="/register" element={<Register/>}/>
+                        <Route path="/forgotPassword" element={<ForgotPassword/>}/>
+                        <Route path="/resetPassword" element={<Verify type={"password"}/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                        <Route path="/privacy"
+                               element={<TextPage>
+                                   <PrivacyPolicy
+                                       appName={"Annual Leave Tracker"}
+                                       url={window.location.pathName}
+                                       websiteName={"Annual Leave Tracker"}
+                                       email={"help@annual-leave-tracker.com"}
+                                   />
+                               </TextPage>}/>
+                        <Route path="*" element={<Navigate to="/login"/>}/>
+                    </Routes></div>
             </div>
         </div>
     </div>);
