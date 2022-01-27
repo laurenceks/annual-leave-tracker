e6ee1bad37b978f4f05bdf558d93314e50b126bf
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-01-27 09:42:45 +0000

    Changed layout of policy pages to cover whole screen

--- src/components/login/policies/TextPage.js
+++ src/components/login/policies/TextPage.js
@@ -1,13 +1,17 @@
 import {NavLink} from "react-router-dom";
 import PropTypes from "prop-types";
 
-const TextPage = ({children, previousRoute}) => {
-    return (
-        <div>
-            {children}
-            <NavLink className="btn btn-primary my-5 w-100" to={previousRoute || "/"}>Go back</NavLink>
+const TextPage = ({
+                      children,
+                      previousRoute
+                  }) => {
+    return (<div className="position-fixed w-100 h-100 overflow-scroll top-0 start-0 p-4 p-md-5 bg-white">
+        {children}
+        <div className="text-center">
+            <NavLink className="btn btn-primary my-5 w-100" to={previousRoute || "/"} style={{maxWidth: "450px"}}>Go
+                back</NavLink>
         </div>
-    );
+    </div>);
 };
 
 TextPage.propTypes = {
