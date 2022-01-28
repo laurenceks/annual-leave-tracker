e6b05b4e46161a1028e3393ea1756c1f406c02d2
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-01-28 15:55:59 +0000

    Added extra check for if given dates are between a period

--- public/php/common/checkFunctions/checkDatesDontOverlap.php
+++ public/php/common/checkFunctions/checkDatesDontOverlap.php
@@ -8,12 +8,14 @@ function checkDatesDontOverlap($table, $from, $to, $includeDeleted = false, $use
     $whiteList = array("tables" => array("bookings", "allowances", "periods", "locations", "pay_grades", "users_info"), "keys" => array("id", "name", "listId", "itemId", "userId"));
 
     if (count($keyValues) > 0 && in_array($table, $whiteList["tables"], true) && in_array($selectKey, $whiteList["keys"], true)) {
-        $check = $db->prepare("SELECT id, dateFrom, dateTo FROM " . $table . " WHERE organisationId = :organisationId AND ((dateFrom >= :dateFrom1 AND dateFrom <= :dateTo1) OR (dateTo >= :dateFrom2 AND dateTo <= :dateTo2))" . ($includeDeleted ? "" : " AND deleted = 0") . ($entryId ? " AND id != :id" : "") . (($userId && $table === "bookings") ? " AND userId = :userId" : "") . " ORDER BY dateFrom");
+        $check = $db->prepare("SELECT id, dateFrom, dateTo FROM " . $table . " WHERE organisationId = :organisationId AND ((dateFrom >= :dateFrom1 AND dateFrom <= :dateTo1) OR (dateTo >= :dateFrom2 AND dateTo <= :dateTo2) OR (dateFrom <= :dateFrom3 AND dateTo >= :dateTo3))" . ($includeDeleted ? "" : " AND deleted = 0") . ($entryId ? " AND id != :id" : "") . (($userId && $table === "bookings") ? " AND userId = :userId" : "") . " ORDER BY dateFrom");
         $getAllBookings->bindValue(':organisationId', $_SESSION["user"]->organisationId);
         $getAllBookings->bindValue(':dateFrom1', $from);
         $getAllBookings->bindValue(':dateFrom2', $from);
+        $getAllBookings->bindValue(':dateFrom3', $from);
         $getAllBookings->bindValue(':dateTo1', $to);
         $getAllBookings->bindValue(':dateTo2', $to);
+        $getAllBookings->bindValue(':dateTo3', $to);
         if ($entryId) {
             $check->bindValue(":id", $entryId);
         }
