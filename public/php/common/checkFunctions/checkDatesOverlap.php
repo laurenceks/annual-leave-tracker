71e3f320af96ad1e672ce0cdf1d7baf10b6ddc35
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-01-28 16:14:40 +0000

    Fixed relative paths and var name typo

--- public/php/common/checkFunctions/checkDatesOverlap.php
+++ public/php/common/checkFunctions/checkDatesOverlap.php
@@ -1,7 +1,7 @@
 <?php
 function checkDatesOverlap($table, $from, $to, $includeDeleted = false, $userId = null, $entryId = null) {
-    require_once "../../security/userLoginSecurityCheck.php";
-    require "../../common/db.php";
+    require_once dirname(__FILE__) . "/../../security/userLoginSecurityCheck.php";
+    require dirname(__FILE__) . "/../../common/db.php";
 
     //returns true if dates overlap - default is to assume overlap and return true
     $response = true;
@@ -10,13 +10,13 @@ function checkDatesOverlap($table, $from, $to, $includeDeleted = false, $userId 
     if (in_array($table, $whiteList["tables"], true)) {
         try {
             $check = $db->prepare("SELECT id, dateFrom, dateTo FROM " . $table . " WHERE organisationId = :organisationId AND ((dateFrom >= :dateFrom1 AND dateFrom <= :dateTo1) OR (dateTo >= :dateFrom2 AND dateTo <= :dateTo2) OR (dateFrom <= :dateFrom3 AND dateTo >= :dateTo3))" . ($includeDeleted ? "" : " AND deleted = 0") . ($entryId ? " AND id != :id" : "") . (($userId && $table === "bookings") ? " AND userId = :userId" : "") . " ORDER BY dateFrom");
-            $getAllBookings->bindValue(':organisationId', $_SESSION["user"]->organisationId);
-            $getAllBookings->bindValue(':dateFrom1', $from);
-            $getAllBookings->bindValue(':dateFrom2', $from);
-            $getAllBookings->bindValue(':dateFrom3', $from);
-            $getAllBookings->bindValue(':dateTo1', $to);
-            $getAllBookings->bindValue(':dateTo2', $to);
-            $getAllBookings->bindValue(':dateTo3', $to);
+            $check->bindValue(':organisationId', $_SESSION["user"]->organisationId);
+            $check->bindValue(':dateFrom1', $from);
+            $check->bindValue(':dateFrom2', $from);
+            $check->bindValue(':dateFrom3', $from);
+            $check->bindValue(':dateTo1', $to);
+            $check->bindValue(':dateTo2', $to);
+            $check->bindValue(':dateTo3', $to);
             if ($entryId) {
                 $check->bindValue(":id", $entryId);
             }
