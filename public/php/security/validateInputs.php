e8b4907996b8e8ff083bad0295f8e634bec2b04f
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-02-03 15:49:30 +0000

    Removed needless error if no value or template

--- public/php/security/validateInputs.php
+++ public/php/security/validateInputs.php
@@ -1,22 +1,28 @@
 <?php
-function validateInputs() {
+function validateInputs($trimInput = true) {
 
     $rawInputs = json_decode(file_get_contents('php://input'), true);
 
     if ($rawInputs && count($rawInputs) > 0) {
         function exitDueToInvalidInput($key) {
-            die(json_encode(["title" => "Invalid inputs",
+            die(json_encode([
+                "success" => false,
+                "title" => "Invalid inputs",
                 "feedback" => "Some of the inputs passed could not be validated",
-                "errorMessage" => "Unable to validate input '" . $key . "'",
-                "errorType" => "invalidInput"]));
+                "errorMessage" => "Unable to validate input '" . ($key || "") . "'",
+                "errorType" => "invalidInput"
+            ]));
         }
 
-        $validatedInputs = [];
-        $expressions = ["string" => ["options" => ["regexp" => "/[\s\S]*/"]],
+        $validatedInputs = ["rawInputs" => $rawInputs];
+        $expressions = [
+            "string" => ["options" => ["regexp" => "/[\s\S]*/"]],
             "date" => ["options" => ["regexp" => "/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/"]],
-            "password" => ["options" => ["regexp" => "/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/"]]];
+            "password" => ["options" => ["regexp" => "/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/"]]
+        ];
 
-        $inputTypes = ["userId" => FILTER_VALIDATE_INT,
+        $inputTypes = [
+            "userId" => FILTER_VALIDATE_INT,
             "maskedEmail" => FILTER_VALIDATE_EMAIL,
             "maskedFirstName" => "string",
             "maskedLastName" => "string",
@@ -61,12 +67,12 @@ function validateInputs() {
             "feedbackVerb" => "string",
             "staffFullName" => "string",
             "locationId" => FILTER_VALIDATE_INT,
-            "payGradeId" => FILTER_VALIDATE_INT,];
+            "payGradeId" => FILTER_VALIDATE_INT,
+        ];
 
         foreach ($rawInputs as $key => $value) {
             if (isset($value) && isset($inputTypes[$key])) {
-                $value = htmlspecialchars($value);
-                $valueIsValid = false;
+                $value = $trimInput && trim($value);
                 if (gettype($inputTypes[$key]) === "string") {
                     $valueIsValid = filter_var($value, FILTER_VALIDATE_REGEXP, $expressions[$inputTypes[$key]]);
                 } else {
@@ -77,17 +83,18 @@ function validateInputs() {
                 } else {
                     exitDueToInvalidInput($key);
                 }
-            } else {
-                exitDueToInvalidInput($key);
             }
         }
 
         return $validatedInputs;
 
     } else {
-        die(json_encode(["title" => "No inputs",
+        die(json_encode([
+            "success" => false,
+            "title" => "No inputs",
             "feedback" => "No inputs were passed to the validation function",
             "errorMessage" => "No inputs were passed to the validation function",
-            "errorType" => "noInputs"]));
+            "errorType" => "noInputs"
+        ]));
     }
 }
