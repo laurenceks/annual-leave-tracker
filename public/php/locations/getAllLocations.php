85fc5c2e0aacbf37f66852c1650c4a9682f958e3
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-02-03 16:14:07 +0000

    Removed unused variable

--- public/php/locations/getAllLocations.php
+++ public/php/locations/getAllLocations.php
@@ -4,7 +4,6 @@ require "../common/db.php";
 require "../common/feedbackTemplate.php";
 
 $output = array_merge($feedbackTemplate, array("locations" => array()));
-$input = json_decode(file_get_contents('php://input'), true);
 $getAllLocations = $db->prepare("
 SELECT 
   *
