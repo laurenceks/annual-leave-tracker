0af23e6a611dc6133a410409030b1a62aa1652a8
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-02-06 12:23:18 +0000

    Query data now sorted according to passed options to make banding rows easier

--- public/php/hours/getHours.php
+++ public/php/hours/getHours.php
@@ -4,54 +4,81 @@ require "../common/db.php";
 require "../common/feedbackTemplate.php";
 require "../security/validateInputs.php";
 
-$output = array_merge($feedbackTemplate, array("bookings" => array()));
+$output = array_merge($feedbackTemplate, ["hours" => []]);
 $input = validateInputs();
 
-if (!isset($input["periodId"])) {
-    require "../periods/getPeriodIdFromDateRange.php";
-    $period = getPeriodIdFromDateRange();
-} else {
-    $period = $input["period"];
+require "hoursQuery.php";
+
+function makeArraySplit($row, $keyId, $keyName, $includeHours = true) {
+    return array_merge([
+        $keyId => $row[$keyId],
+        $keyName => $row[$keyName]
+    ], $includeHours ? [
+        "hours" => $row["hours"],
+        "approvedBookings" => $row["approvedBookings"]
+    ] : []);
 }
 
-if (isset($period["id"])) {
-    require "dashboardChartMonthQuery.php";
-    require "../allowances/getAllowanceForUserByPeriod.php";
+try {
+    $getHours = $db->prepare($hoursByDateSplitQuery);
 
-    $getChartMonths = $db->prepare($dashboardChartMonthQuery);
+    $getHours->bindValue(':organisationId', $_SESSION["user"]->organisationId);
+    $getHours->bindValue(':minDate', $input["from"]);
+    $getHours->bindValue(':maxDate', $input["to"]);
+    $getHours->bindValue(':interval', $input["dateGroup"]);
+    $getHours->bindValue(':weekDayStart', $input["weekStart"]);
+    $getHours->bindValue(':splitByPayGrade', $input["splitByPayGrade"]);
+    $getHours->bindValue(':splitByLocation', $input["splitByLocation"]);
+    $getHours->bindValue(':groupSplitBy', $input["groupSplitBy"]);
+    $getHours->execute();
 
-    $getChartMonths->bindValue(':userId', $_SESSION["user"]->userId);
-    $getChartMonths->bindValue(':organisationId', $_SESSION["user"]->organisationId);
-    $getChartMonths->bindValue(':dateFrom', $period["dateFrom"]);
-    $getChartMonths->bindValue(':dateTo', $period["dateTo"]);
-    $getChartMonths->execute();
+    if ($input["splitByLocation"] || $input["splitByPayGrade"]) {
+        $secondSplit = $input["splitByPayGrade"] && $input["splitByLocation"];
+        $splitKeyId = $secondSplit ? $input["groupSplitBy"] : ($input["splitByLocation"] ? "locationId" : "payGradeId");
+        $splitKeyName = $splitKeyId === "locationId" ? "locationName" : "payGradeName";
+        $altKeyId = $splitKeyId === "locationId" ? "payGradeId" : "locationId";
+        $altKeyName = $splitKeyId === "locationId" ? "payGradeName" : "locationName";
+        $previousRow = ["date" => null, $splitKeyId => null];
 
-    $output["chartData"]["chartMonths"] = $getChartMonths->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($getHours as $row) {
+            if ($previousRow["date"] === $row["date"]) {
+                $lastRowIndex = count($output["hours"]) - 1;
+                if ($secondSplit) {
+                    $lastSplitIndex = isset($output["hours"][$lastRowIndex]["split"]) ? (count($output["hours"][$lastRowIndex]["split"]) - 1) : 0;
+                    if ($previousRow[$splitKeyId] === $row[$splitKeyId]) {
+                        $output["hours"][$lastRowIndex]["split"][$lastSplitIndex]["split"][] = makeArraySplit($row, $altKeyId, $altKeyName);
+                    } else {
+                        $output["hours"][$lastRowIndex]["split"][] = array_merge(makeArraySplit($row, $splitKeyId, $splitKeyName, false), [
+                            "split" => [makeArraySplit($row, $altKeyId, $altKeyName)]
+                        ]);
+                    }
+                } else {
+                    $output["hours"][$lastRowIndex]["split"][] = makeArraySplit($row, $splitKeyId, $splitKeyName);
+                }
+            } else {
+                $output["hours"][] = [
+                    "date" => $row["date"],
+                    "split" => [
+                        array_merge(makeArraySplit($row, $splitKeyId, $splitKeyName, !$secondSplit), $secondSplit ? [
+                            "split" => [makeArraySplit($row, $altKeyId, $altKeyName)]
+                        ] : [])
+                    ]
+                ];
+            }
+            $previousRow = $row;
+        }
+    } else {
+        $output["hours"] = $getHours->fetchAll(PDO::FETCH_ASSOC);
+    }
 
-
-    $output["period"] = $period;
-
-    $allowanceForIdAndPeriod = getAllowanceForUserByPeriod($period);
-    $output["allowance"] = $allowanceForIdAndPeriod;
-
-    $output["chartData"]["chartHours"] = [["label" => "Remaining", "hours" => isset($allowanceForIdAndPeriod["remaining"]) ? $allowanceForIdAndPeriod["remaining"] : 0], ["label" => "Booked", "hours" => (isset($allowanceForIdAndPeriod["requested"]) ? $allowanceForIdAndPeriod["requested"] : 0) + (isset($allowanceForIdAndPeriod["approved"]) ? $allowanceForIdAndPeriod["approved"] : 0)], ["label" => "Taken", "hours" => isset($allowanceForIdAndPeriod["taken"]) ? $allowanceForIdAndPeriod["taken"] : 0]];
-
-    $getBookings = $db->prepare("SELECT * FROM `bookings` WHERE `dateFrom` >= :dateFrom AND `dateTo` <= :dateTo AND `userId` = :userId AND `organisationId`= :organisationId AND `deleted` = 0");
-    $getBookings->bindValue(':userId', $_SESSION["user"]->userId);
-    $getBookings->bindValue(':organisationId', $_SESSION["user"]->organisationId);
-    $getBookings->bindValue(':dateFrom', $period["dateFrom"]);
-    $getBookings->bindValue(':dateTo', $period["dateTo"]);
-    $getBookings->execute();
-
-    $output["bookings"] = $getBookings->fetchAll(PDO::FETCH_ASSOC);
-
-
-} else {
-    $output["noPeriod"] = true;
+    $output["success"] = true;
+    $output["title"] = "Hours updated";
+    $output["feedback"] = "Now showing data for the period XXXX";
+} catch (PDOException $e) {
+    $output = array_merge($output, array(
+        "feedback" => "There was an error querying the database; please try again. If the error persists please contact a system administrator for assistance.",
+        "errorMessage" => "There was an error querying the database; please try again. If the error persists please contact a system administrator for assistance.",
+        "errorType" => "queryError"
+    ));
 }
-
-$output["success"] = true;
-$output["title"] = "Data updated";
-$output["feedback"] = isset($period["id"]) ? "Now showing data for the period " . $period["name"] : "No matching period could be found";
-
-echo json_encode($output, JSON_NUMERIC_CHECK);
+echo json_encode($output);
