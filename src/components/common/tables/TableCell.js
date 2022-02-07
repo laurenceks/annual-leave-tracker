688d600a0ab53f001682ece358afa9cfae7e2cbc
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-02-07 13:22:33 +0000

    Hover applied if classname contains hovergroup.current

--- src/components/common/tables/TableCell.js
+++ src/components/common/tables/TableCell.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import {IoCheckmarkCircleSharp, IoCloseCircleSharp} from "react-icons/io5";
 import FormInput from "../forms/FormInput";
+import {useRef} from "react";
 
 const renderCellContent = (x) => {
     if (!x || !x.type) {
@@ -9,47 +10,47 @@ const renderCellContent = (x) => {
         switch (x.type) {
             case "tick":
             case "tick-invert":
-                return (
-                    <span
-                        className={`tableIcon ${x.type !== "tick-invert" ? (x.value ? "text-success" : "text-danger") : !x.value ? "text-success" : "text-danger"}`}>
-                        {x.value ? <IoCheckmarkCircleSharp/> :
-                            <IoCloseCircleSharp/>}
-                    </span>
-                )
+                return (<span className={`tableIcon ${x.type !== "tick-invert" ?
+                    (x.value ? "text-success" : "text-danger") :
+                    !x.value ? "text-success" : "text-danger"}`}>
+                        {x.value ? <IoCheckmarkCircleSharp/> : <IoCloseCircleSharp/>}
+                    </span>)
             case "button":
             case "submit":
-                return (
-                    <button type={x.type === "submit" ? "submit" : "button"}
-                            form={x.type === "submit" && x.form ? x.form : ""}
-                            className={`btn ${x.buttonClass || "btn-primary"}`} onClick={x.handler}
-                            data-userid={x.id}>{x.text}</button>
-                )
+                return (<button type={x.type === "submit" ? "submit" : "button"}
+                                form={x.type === "submit" && x.form ? x.form : ""}
+                                className={`btn ${x.buttonClass || "btn-primary"}`}
+                                onClick={x.handler}
+                                data-userid={x.id}>{x.text}</button>)
             case "input":
-                return (
-                    <FormInput {...x.props}/>
-                )
+                return (<FormInput {...x.props}/>)
             default:
                 return x.text ?? "";
         }
     }
 }
 
-const TableCell = ({className, content, align, hoverGroup}) => {
-    return (
-        <td className={`${align && align + " "} ${content?.cellData?.["data-rowGroupId"] === hoverGroup.current && hoverGroup.current && "hover"} ${className || ""}`}
-            colSpan={content?.colspan}
-            rowSpan={content?.rowspan}
-            onMouseEnter={() => {
-                hoverGroup.set(content?.cellData?.["data-rowGroupId"])
-            }}
-            onMouseLeave={() => {
-                hoverGroup.set(null)
-            }}
-            {...content?.cellData}
-        >
-            {content?.fragment ?? renderCellContent(content)}
-        </td>
-    );
+const TableCell = ({
+                       className,
+                       content,
+                       align,
+                       hoverGroup
+                   }) => {
+    const hoverMatch = (content?.cellData?.["data-rowGroupId"] === hoverGroup.current || className.includes(
+        hoverGroup.current)) && hoverGroup.current;
+    return (<td className={`${align} ${hoverMatch ? "hover" : ""} ${className || ""}`}
+                colSpan={content?.colspan}
+                rowSpan={content?.rowspan}
+                onMouseEnter={() => {
+                    hoverGroup.set(content?.cellData?.["data-rowGroupId"])
+                }}
+                onMouseLeave={() => {
+                    hoverGroup.set(null)
+                }}
+                {...content?.cellData}
+    >
+        {content?.fragment ?? renderCellContent(content)}
+    </td>);
 };
 
 TableCell.propTypes = {
